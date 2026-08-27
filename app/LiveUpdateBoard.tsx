4b1bf30065479cc943f154692e7d14052f484519
Zero <silverfantacy@gmail.com>
2026-08-28 01:52:40 +0800

    fix: set live marquee speed to forty

--- app/LiveUpdateBoard.tsx
+++ app/LiveUpdateBoard.tsx
@@ -45,7 +45,7 @@ function LiveMarqueeRow({people,reverse=false,onNavigate}:{people:StaffSummary[]
         if(interacting.current||Date.now()<pausedUntil.current){
           position=row.scrollLeft;
         }else{
-          position+=(reverse?-1:1)*elapsed*.036;
+          position+=(reverse?-1:1)*elapsed*.04;
           if(position>=width*2)position-=width;
           else if(position<=0)position+=width;
           row.scrollLeft=position;
